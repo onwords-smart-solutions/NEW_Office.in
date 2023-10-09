ed528783dc4f2e4bc3cf485e0b2defc75d574eff
srvinith <r36041595@gmail.com>
2023-10-09 16:52:58 +0530

    monday ui

--- adminconsole/static/js/main.js
+++ adminconsole/static/js/main.js
@@ -9,7 +9,7 @@ function navcon() {
         for (const rightnav of rightnavlist) {
             rightnav.style.display = 'block'
         }
-        smallLogo.style.visibility='hidden'
+        smallLogo.style.visibility = 'hidden'
 
     }
     else {
@@ -18,7 +18,7 @@ function navcon() {
         for (const rightnav of rightnavlist) {
             rightnav.style.display = 'none'
         }
-        smallLogo.style.visibility='visible'
+        smallLogo.style.visibility = 'visible'
     }
 }
 function rightcon() {
@@ -31,19 +31,65 @@ function rightcon() {
     for (const rightnav of rightnavlist) {
         rightnav.style.display = 'none'
     }
-    smallLogo.style.visibility='visible'
+    smallLogo.style.visibility = 'visible'
 
 }
 
-
 // ===============activd nav start============
+
 var header = document.getElementById("nav-main-con");
 var btns = header.getElementsByClassName("navlistmenu");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("navlistmenu-active");
-  current[0].className = current[0].className.replace(" navlistmenu-active", "");
-  this.className += " navlistmenu-active";
-  });
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("navlistmenu-active");
+        current[0].className = current[0].className.replace(" navlistmenu-active", "");
+        this.className += " navlistmenu-active";
+    });
 }
 // ===============activd nav end============
+
+
+// ===========check order button==============
+
+const checkOrder = document.querySelectorAll('.check-order');
+const orderButton = document.querySelector('#ordBtn');
+
+checkOrder.forEach(chekBtn => {
+    chekBtn.addEventListener('change', () => {
+        const atLeastOneChecked = Array.from(checkOrder).some(chk => chk.checked);
+        if (atLeastOneChecked) {
+            orderButton.style.display = 'block';
+        } else {
+            orderButton.style.display = 'none';
+        }
+    });
+});
+
+// ===========check order button============== 
+
+
+// break
+var xValues = [];
+      var yValues = [ 30,70];
+      var barColors = [
+          "#7FA9FB",
+          "#266386",
+      ];
+      new Chart("myChart2", {
+          type: "doughnut",
+          data: {
+              labels: xValues,
+              datasets: [{
+                  backgroundColor: barColors,
+                  data: yValues,
+                  borderColor: 'transparent'
+              }]
+          },
+          options: {
+              cutoutPercentage: 80,
+              title: {
+                  display: true,
+                  text: ""
+              }
+          }
+      });
