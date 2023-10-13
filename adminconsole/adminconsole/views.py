3ad18b91e778ecdeabea4673629c75ee8c7db728
Gokilavani-A <133311326+Gokilavani-A@users.noreply.github.com>
2023-10-13 09:41:22 +0530

    add changes

--- adminconsole/adminconsole/views.py
+++ adminconsole/adminconsole/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def login(request):
     
-    return render(request,'signin.html')
+    return render(request,'login.html')
 def leave_form(request):
     return render(request,'leave-form.html')
 def late_form(request):
