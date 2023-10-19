4b9ee35865a6ac79741ee8b4c04a6a3743571ead
srvinith <r36041595@gmail.com>
2023-10-19 18:02:15 +0530

    access potal ui changes

--- adminconsole/superadmin/views.py
+++ adminconsole/superadmin/views.py
@@ -7,3 +7,9 @@ def createstaff(request):
     return render(request,'createstaff.html')
 def staffaccess(request):
     return render(request,'staff-access.html')
+def userdata(request):
+    return render(request,'userdata.html')
+def viewsuggestion(request):
+    return render(request,'viewsuggestion.html')
+def viewworkmanager(request):
+    return render(request,'viewworkmanager.html')
