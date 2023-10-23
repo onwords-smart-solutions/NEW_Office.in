6b6e25afa37f188264ed8c67774452481bf4a109
sanjeev929 <sanjeevsanju929@gmail.com>
2023-10-23 11:58:19 +0530

    add create staff

--- adminconsole/superadmin/views.py
+++ adminconsole/superadmin/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from adminconsole.views import db, checkUserName
-
+from django.http import HttpResponse, JsonResponse
+from adminconsole.views import db, auth, checkUserName
 # Create your views here.
 def superadmin(request):
     uid = request.COOKIES["uid"]
@@ -12,10 +13,50 @@ def superadmin(request):
         "dep":dep,
     }
     return render(request,'superadmin.html',context)
+
 def createstaff(request):
-    return render(request,'createstaff.html')
+    suggestionNotification = 0
+    suggestionData = db.child("suggestion").get().val()
+    for suggestion in suggestionData:
+            if not suggestionData[suggestion]["isread"]:
+                suggestionNotification += 1
+
+    if request.method == "POST":
+        try:
+            _name = request.POST["name"]
+            _email = request.POST["email"]
+            _department = request.POST["department"]
+            _password = request.POST["password"]
+            try:
+                _pt = request.POST["pt"]
+                data = {
+                    "name": _name,
+                    "email": _email,
+                    "department": _department,
+                    "partTime": True,
+                }
+            except:
+                data = {
+                    "name": _name,
+                    "email": _email,
+                    "department": _department,
+                }
+            usr = auth.create_user_with_email_and_password(_email,_password)
+            db.child("staff").child(usr["localId"]).set(data)
+            auth.send_password_reset_email(_email)
+            return render(
+                request,
+                "createstaff.html",
+                {"message": "User Created Successfully"},
+            )
+        except:
+            return HttpResponse("error creating user")
+    else:
+        return render(request, "createstaff.html",{"suggestionNotification":suggestionNotification})
+
 def staffaccess(request):
     return render(request,'staff-access.html')
+
 def userdata(request):
     return render(request,'userdata.html')
 def viewsuggestion(request):
