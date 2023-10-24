675489d748556b04ea02633f615d8f6e246b31ca
Isaak29 <akmohamedisaakmuzamil29@gmail.com>
2023-10-24 11:44:40 +0530

    installation add

--- adminconsole/it/views.py
+++ adminconsole/it/views.py
@@ -76,7 +76,7 @@ def ithome(request):
         try:
             if yesterday.weekday() == 6:
                 yescheckout = attendence[saturday_year][saturday_month][saturday_day][uid]["check_out"]
-                yesscheckout = convert_to_12_hour_format(yescheckin)
+                yesscheckout = convert_to_12_hour_format(yescheckout)
             else:    
                 yescheckout = attendence[yesterday_year][yesterday_month][yesterday_day][uid]["check_out"]
                 yesscheckout = convert_to_12_hour_format(yescheckout)
